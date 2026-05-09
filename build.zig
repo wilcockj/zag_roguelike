6959cc86ae9236a4ec5f56c00e5763bbd6a85c47
Aiden Olsen <olsenaiden33@gmail.com>
2026-05-09 12:06:25 -0700

    waywib

--- build.zig
+++ build.zig
@@ -41,6 +41,15 @@ pub fn build(b: *std.Build) void {
         .target = target,
     });
 
+    const raylib_dep = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raygui = raylib_dep.module("raygui"); // raygui module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+
     // Here we define an executable. An executable needs to have a root module
     // which needs to expose a `main` function. While we could add a main function
     // to the module defined above, it's sometimes preferable to split business
@@ -82,6 +91,10 @@ pub fn build(b: *std.Build) void {
             },
         }),
     });
+
+    exe.root_module.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
 
     // This declares intent for the executable to be installed into the
     // install prefix when running `zig build` (i.e. when executing the default
