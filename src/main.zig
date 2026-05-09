390d2170d72f57e220d1b01f76a4a1483bee07bc
Aiden Olsen <olsenaiden33@gmail.com>
2026-05-09 12:54:36 -0700

    added game state

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,29 @@ const card = @import("card.zig");
 
 const zag_roguelike = @import("zag_roguelike");
 
+const State = enum {
+    menu,
+    running,
+};
+
+const Game = struct {
+    allocator: std.mem.Allocator,
+    state: State,
+    cards: std.ArrayList(card.Card),
+
+    pub fn init(allocator: std.mem.Allocator) Game {
+        return Game{
+            .allocator = allocator,
+            .state = .menu,
+            .cards = .empty,
+        };
+    }
+
+    pub fn deinit(self: *Game) void {
+        self.cards.deinit(self.allocator);
+    }
+};
+
 pub fn main(init: std.process.Init) !void {
     _ = init;
 
@@ -13,10 +36,8 @@ pub fn main(init: std.process.Init) !void {
 
     const allocator = arena.allocator();
 
-    var cards: std.ArrayList(card.Card) = .empty;
-    defer cards.deinit(allocator);
-
-    try cards.append(allocator, card.Card.init("test"));
+    var game = Game.init(allocator);
+    defer game.deinit();
 
     rl.initWindow(800, 450, "game");
 
@@ -27,7 +48,7 @@ pub fn main(init: std.process.Init) !void {
         rl.clearBackground(rl.getColor(0x181818ff));
         rl.drawText("hello zag", 0, 0, 12, .white);
 
-        for (cards.items) |c| {
+        for (game.cards.items) |c| {
             c.draw();
         }
     }
